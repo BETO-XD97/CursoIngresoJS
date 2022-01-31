6d81e74e85db56d6e890edb7b1cd61830bf989d6
BETO-XD97 <93448643+BETO-XD97@users.noreply.github.com>
2022-01-31 12:31:03 -0300

    ejercicios del 1 al 5

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,11 @@
 /*
+Roberto Rocabado DIV E
+E/S ej1 
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	//muestra ventana emergente
+	alert("hola");
 }
 
--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -1,9 +1,23 @@
 /*
+Roberto Rocabado DIV E
+E/J ej6
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	//declaro variables
+	var n1;
+	var n2;
+	var suma
+
+	//ingreso de datos
+	n1 = document.getElementById("txtIdNumeroUno").value;
+	n1 = parseInt("txtIdNumeroUno");
+	n2 = document.getElementById("txtIdNumeroDos").value;
+	n2 = parseInt("txtIdNumeroDos");
+
+	//ventana emergente
+	alert("")
 }
 
